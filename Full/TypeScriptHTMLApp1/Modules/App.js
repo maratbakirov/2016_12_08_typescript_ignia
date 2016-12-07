460ab4b378c6a8dd3c541637be2d0a0bda60e1db
mbakirov <mbakirov@hotmail.com>
2016-12-07 23:49:19 +0800

    working modules sampel

--- Full/TypeScriptHTMLApp1/Modules/App.js
+++ Full/TypeScriptHTMLApp1/Modules/App.js
@@ -1,9 +1,10 @@
 /// <reference path="../Scripts/typings/knockout/knockout.d.ts"/>
-/// <reference path="../Scripts/typings/jquery/jquery.d.ts"/>
 define(["require", "exports", "./Module1"], function (require, exports, M1) {
     "use strict";
-    window.onload = function () {
-        $("#content").text(new M1.Module1.StringValidator().isAcceptable("fff"));
-    };
+    function Minit() {
+        document.getElementById('content').innerText =
+            new M1.Module1.StringValidator().isAcceptable("fff").toString();
+    }
+    exports.Minit = Minit;
 });
 //# sourceMappingURL=App.js.map
--- Full/TypeScriptHTMLApp1/Modules/App.ts
+++ Full/TypeScriptHTMLApp1/Modules/App.ts
@@ -1,9 +1,9 @@
 ﻿/// <reference path="../Scripts/typings/knockout/knockout.d.ts"/>
-/// <reference path="../Scripts/typings/jquery/jquery.d.ts"/>
 
 //import {Module1 as M1} from "./Module1";
 import  M1  = require("./Module1");
 
-window.onload = () => {
-    $("#content").text(new M1.Module1.StringValidator().isAcceptable("fff"));
+export function Minit() {
+    document.getElementById('content').innerText =
+        new M1.Module1.StringValidator().isAcceptable("fff").toString();
 }
